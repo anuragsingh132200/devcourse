8608e164a8c1d2e189fab2cbd53ac6670bf88748
anuragsingh132200 <anurag132200@gmail.com>
2023-12-10 17:37:33 +0530

    this is a list of my devcourse projects

---
+++ starting node/index.js
@@ -0,0 +1,7 @@
+// var generatename = require('sillyname');
+import generatename from 'sillyname';
+var name = generatename();
+console.log('hello  ' + name + '!');
+import superheroes from 'superheroes';
+var name = superheroes.random();
+console.log('hello  ' + name + '!');
